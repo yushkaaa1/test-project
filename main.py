6f8841643a6e48f9fcd1cf110f71a6d5b7b717e4
Sasha Yushkova <36802673-alexachuch50@users.noreply.replit.com>
2024-10-30 07:51:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# import random
+
+# stone = 1
+# scissors = 2
+# paper = 3
+# print("Welcome to the game of stone, paper, scissors")
+# print("stone=1, scissors=2, paper=3")
+
+# users_choice = int(input("What is your choice?:"))
+# computers_choice = random.randint(1,3)
+# print(computers_choice)
+
+
+# if users_choice == 1 and computers_choice == 2:
+#     print("user win!")
+# elif users_choice == 1 and computers_choice == 1:
+#     print("draw")
+# elif (users_choice == 1 and computers_choice == 3) or (users_choice == 2 and computers_choice == 1):
+#     print("computer win")
+# elif users_choice == 2 and computers_choice == 2:
+#     print("draw")
+# elif (users_choice == 2 and computers_choice == 3) or (users_choice == 3 and computers_choice == 1):
+#     print("user win")
+# elif users_choice == 3 and computers_choice == 2:
+#     print("computer win")
+# elif (users_choice == 3 and computers_choice == 3) or (users_choice == 2 and computers_choice == 2):
+#     print("draw")
+# elif (users_choice == 2 and computers_choice == 3) or (users_choice == 3 and computers_choice == 1):
+#     print("user win")
+# elif users_choice == 3 and computers_choice == 2:
+#     print("computer win")
+# elif users_choice == 3 and computers_choice == 3:
+#     print("draw")
